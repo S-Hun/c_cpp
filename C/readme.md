bd989f49c400e4268d5eed7c6c4c9ec8d2ec3390
S-Hun <qotjrgnsg@gmail.com>
2022-09-06 22:16:05 +0900

    Add C, C++, C# Languages directory, hello world examples

---
+++ C/readme.md
@@ -0,0 +1,1 @@
+hello world [예제](./source000.c)
---
+++ Csharp/source000.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Project
+{
+    class Program 
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello, World!\n");
+        }
+    }
+}
